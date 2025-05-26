37367e9b19b6b61155ca602de148935862f0da86
github-AmanSinghRajput <rajput.aman900@gmail.com>
2025-05-26 21:02:11 +0530

    Implement Kadane-style solution for Maximum Product Subarray with O(n) time and constant space

--- Arrays/Medium/MaximumProductSubarray.java
+++ Arrays/Medium/MaximumProductSubarray.java
@@ -1,4 +1,23 @@
 package Arrays.Medium;
 
 public class MaximumProductSubarray {
+    public int maxProduct(int[] nums) {
+        int result = nums[0];
+        int minProduct = nums[0];
+        int maxProduct = nums[0];
+        for(int i=1;i<nums.length; i++){
+            int current = nums[i];
+            // store previous values before updating
+            int tempMax = maxProduct;
+
+            maxProduct = Math.max(current, Math.max(current * maxProduct, current*minProduct));
+            minProduct = Math.min(current, Math.min(current * tempMax, current * minProduct));
+            result = Math.max(result,maxProduct);
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+
+    }
 }
